01268d655ae21afd99114680f72ec22ff7993462
riley soloner <rileysoloner@gmail.com>
2022-08-13 19:32:14 -0400

    commit tree 918479758618a377e50817897c7849f4393b45a5

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
+  end
+  
+  def question?
+    self.end_with?("?")
     
   end
-
-  def question?
-
-  end
-
+  
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    endings = [".", "!", "?"]
+    sentences = 0
+    self.chars.each.with_index do |char, index|
+      if endings.include?(char) && !endings.include?(self[index + 1])
+        sentences += 1
+      end
+    end
+    sentences
+    
+    
   end
 end
